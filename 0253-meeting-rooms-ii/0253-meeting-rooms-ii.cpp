a9d67360e35005e70328974cf2958075a69233f3
leetcode1111 <147308037+leetcode1111@users.noreply.github.com>
2023-10-08 15:17:54 +0530

    Time: 19 ms (29.9%), Space: 12 MB (71.93%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
 int minMeetingRooms(vector<vector<int>>& intervals) {
-    vector<pair<int,int>> arr;
-    // 
-    //  
-    for(int i=0; i<intervals.size(); i++){
-        arr.push_back({intervals[i][0], 1});
-        arr.push_back({intervals[i][1], -1});
-    }
-    sort(arr.begin(), arr.end());
-    int ans=0, cur=0;
-    for(int i=0; i<2*intervals.size(); i++){
-        cur+=arr[i].second;
-        ans=max(ans,cur);
+    sort(intervals.begin(), intervals.end());
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    int ans=1;
+    pq.push({intervals[0][1], intervals[0][0]}); // (end time, start time);
+    for(int i=1; i<intervals.size(); i++){
+        if(pq.top().first <= intervals[i][0]){
+            pq.pop();
+        }
+        pq.push({intervals[i][1], intervals[i][0]});
+        ans=max(ans, (int)pq.size());
     }
     return ans;
 }
