d5b238102e77f9bd5d5c96cfadb39e6efd171cbc
leetcode1111 <147308037+leetcode1111@users.noreply.github.com>
2023-10-18 14:01:14 +0530

    Time: 128 ms (84.02%), Space: 65.4 MB (61.54%) - LeetHub

--- 0425-word-squares/0425-word-squares.cpp
+++ 0425-word-squares/0425-word-squares.cpp
@@ -1,5 +1,7 @@
 struct Trie{
+    // storing 26 english alphabets
     vector<Trie*> arr;
+    // for storing address of all words with proper prefix
     vector<const string *> startsWith;
     Trie(){
         arr.resize(26, NULL);
@@ -23,10 +25,10 @@ public:
                 cur->arr[ind] = new Trie();
             }
             cur = cur->arr[ind];
+            // storing address of full word which start from 
             cur->startsWith.push_back(&key);
         }
     }
-
     vector<const string *> searchAllPrefix(const string &key){
         vector<string> res;
         Trie *cur=root;
@@ -42,19 +44,23 @@ public:
 };
 
 void dfs(TrieDs &ds, vector<vector<string>> &res, vector<string> &cur){
+    // base case if cur size is equal to max size return
     if(cur.size() == cur[0].size()){
         res.push_back(cur);
         return;
     }
     
+    // obtaing prefix for current allotment
     string prefix = "";
     int ind = cur.size();
     for(const string &w:cur){
         prefix.push_back(w[ind]);
     }
 
+    // obtaining address of all words starts from prefix
     vector<const string *> wordList = ds.searchAllPrefix(prefix);
     
+    // iterating and backtracking for every word
     for(const string *s: wordList){
         cur.push_back((*s));
         dfs(ds, res, cur);
@@ -67,6 +73,8 @@ vector<vector<string>> myWordSquares(vector<string>& words){
     vector<vector<string>> res;
     TrieDs ds(words);
     vector<string> cur;
+
+    // consider every word as starting point one by one
     for(int i=0; i<words.size(); i++){
         cur.push_back(words[i]);
         dfs(ds, res, cur);
@@ -74,6 +82,8 @@ vector<vector<string>> myWordSquares(vector<string>& words){
     }
     return res;
 }
+
+
 class Solution {
 public:
 vector<vector<string>> wordSquares(vector<string>& words) {
