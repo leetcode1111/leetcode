3d26555afbb706c02159d2b5e4d9efabd1f01e66
leetcode1111 <147308037+leetcode1111@users.noreply.github.com>
2023-10-19 00:49:54 +0530

    Time: 38 ms (48.31%), Space: 31.6 MB (55.62%) - LeetHub

---
+++ 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+vector<string> findStrobogrammatic(int n, bool isOuterMost=true) {
+    if(n==0){
+        return {""};
+    }
+    if(n==1){
+        return {"0","1","8"};
+    }
+    vector<string> res;
+    for(const string &str:findStrobogrammatic(n-2, false)){
+        if(!isOuterMost){
+            res.push_back("0"+str+"0");
+        }
+        res.push_back("1"+str+"1");
+        res.push_back("8"+str+"8");
+        res.push_back("6"+str+"9");
+        res.push_back("9"+str+"6");
+    }
+    return res;
+}
+};
