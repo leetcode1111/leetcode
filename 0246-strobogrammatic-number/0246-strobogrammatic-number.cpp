6b381514ace8a805a4d4801645572a35a94a60bc
leetcode1111 <147308037+leetcode1111@users.noreply.github.com>
2023-10-22 17:55:08 +0530

    Time: 0 ms (100%), Space: 6.6 MB (6.08%) - LeetHub

--- 0246-strobogrammatic-number/0246-strobogrammatic-number.cpp
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.cpp
@@ -8,12 +8,7 @@ bool isStrobogrammatic(const string &num) {
     um['6']='9';
     um['8']='8';
     um['9']='6';
-    if(n%2!=0){
-        if(!(num[n/2]=='0' || num[n/2]=='1' || num[n/2]=='8')){
-            return false;
-        }
-    }
-    for(int i=0; i<(n/2); i++){
+    for(int i=0; i<((n+1)/2); i++){
         if(um[num[i]]!=num[n-1-i]){
             return false;
         }
