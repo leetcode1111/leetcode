b266a07be987cc4a3a7fcdc2c69176cc2a4e32ed
leetcode1111 <147308037+leetcode1111@users.noreply.github.com>
2023-10-25 16:18:46 +0530

    Time: 23 ms (15.64%), Space: 13.8 MB (21.03%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-// int minMeetingRooms(vector<vector<int>>& intervals) {
-//     vector<pair<int,int>> arr;
-//     for(int i=0; i<intervals.size(); i++){
-//         arr.push_back({intervals[i][0], 1});
-//         arr.push_back({intervals[i][1], -1});
-//     }
-//     sort(arr.begin(), arr.end());
-//     int ans=0, cur=0;
-//     for(int i=0; i<2*intervals.size(); i++){
-//         cur+=arr[i].second;
-//         ans=max(ans,cur);
-//     }
-//     return ans;
-// }
 int minMeetingRooms(vector<vector<int>>& intervals) {
-    sort(intervals.begin(), intervals.end());
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-    int ans=1;
-    pq.push({intervals[0][1], intervals[0][0]}); // (end time, start time);
-    for(int i=1; i<intervals.size(); i++){
-        if(pq.top().first <= intervals[i][0]){
-            pq.pop();
-        }
-        pq.push({intervals[i][1], intervals[i][0]});
-        ans=max(ans, (int)pq.size());
+    map<int,int> mp;
+    for(const vector<int> &interval:intervals){
+        mp[interval[0]]++;
+        mp[interval[1]]--;
     }
-    return ans;
+    int res=0, c=0;
+    for(const pair<int,int> &p:mp){
+        c+=p.second;
+        res=max(res,c);
+    }
+    return res;
 }
+
 };
